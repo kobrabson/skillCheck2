913c48c7cd6c218275b69e6053c4436d32c9a3d9
kobrabson <67922399+kobrabson@users.noreply.github.com>
2020-08-07 11:40:56 -0600

    Components imported

---
+++ src/Components/Product/Product.js
@@ -0,0 +1,14 @@
+
+import React, { Component } from 'react'
+
+export class Product extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Product</h2>
+            </div>
+        )
+    }
+}
+
+export default Product

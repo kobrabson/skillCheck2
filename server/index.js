d529a458922616047e685a0a51e9ce35aaf62043
kobrabson <67922399+kobrabson@users.noreply.github.com>
2020-08-07 12:11:07 -0600

    Database connected

--- server/index.js
+++ server/index.js
@@ -1,12 +1,30 @@
+require('dotenv').config();
+
 const express = require('express');
+
+const massive = require('massive')
 const ctrl = require('./controller');
 const app = express();
+
+const { PORT, CONNECTION_STRING } = process.env;
+
+massive({
+    connectionString: CONNECTION_STRING,
+    ssl: {
+        rejectUnauthorized: false
+    }
+})
+.then(db => {
+    app.set('db', db)
+    console.log('db connected roger roger')
+})
+.catch(err => console.log(err));
 
 
 
 app.use(express.json());
 
-const PORT = 4000;
+
 
 app.listen(PORT, () => {
     console.log(`we've lost server ${PORT}`);
